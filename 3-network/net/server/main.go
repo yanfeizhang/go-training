e097233e40b55d0f6d410a39bf3b61cec605bbda
yanfeizhang <yanfeizhang@tencent.com>
2022-03-20 18:43:02 +0800

    fix: 还是给net包的server demo 加个协程处理吧

--- 3-network/net/server/main.go
+++ 3-network/net/server/main.go
@@ -5,6 +5,27 @@ import (
 	"net"
 )
 
+func process(conn net.Conn) {
+	//关闭连接
+	defer conn.Close()
+
+	//读取连接上的数据
+	var buf [1024]byte
+	len, err := conn.Read(buf[:])
+	if len == 0 || err != nil {
+		fmt.Println("accept failed, err:%v\n", err)
+	}
+	fmt.Println("accept data:%v\n", string(buf[0:len]))
+
+	//发送数据
+	_, err = conn.Write([]byte("I am server!"))
+	if err != nil {
+		fmt.Println("send data failed, err:%v\n", err)
+	}
+	fmt.Println("send data success: I am server!")
+
+}
+
 func main() {
 	//构造一个listener
 	listener, _ := net.Listen("tcp", "127.0.0.1:9008")
@@ -18,22 +39,6 @@ func main() {
 			continue
 		}
 
-		//读取连接上的数据
-		var buf [1024]byte
-		len, err := conn.Read(buf[:])
-		if len == 0 || err != nil {
-			fmt.Println("accept failed, err:%v\n", err)
-		}
-		fmt.Println("accept data:%v\n", string(buf[0:len]))
-
-		//发送数据
-		_, err = conn.Write([]byte("I am server!"))
-		if err != nil {
-			fmt.Println("send data failed, err:%v\n", err)
-		}
-		fmt.Println("send data success: I am server!")
-
-		//关闭连接
-		conn.Close()
+		go process(conn)
 	}
 }
