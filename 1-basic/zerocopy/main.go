122e149e40703e0881950b244f192c577981cc6d
zhangyanfei.allen <zhangyanfei.allen@bytedance.com>
2025-07-09 17:40:48 +0800

    feat:zero copy

--- 1-basic/zerocopy/main.go
+++ 1-basic/zerocopy/main.go
@@ -21,35 +21,30 @@ func StringToString(s string) string {
 	return newStr
 }
 
-// BytesToString 零拷贝将[]byte转换为字符串
-func BytesToString(b []byte, offset int) string {
-	// 获取字节切片的底层结构指针
+// ZeroCopyBytesToString: zero copy []byte->string
+func ZeroCopyBytesToString(b []byte) string {
 	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-
-	// 构造字符串的底层结构，复用切片的Data和Len
 	strHeader := reflect.StringHeader{
-		Data: sliceHeader.Data + uintptr(offset),
-		Len:  sliceHeader.Len - offset,
+		Data: sliceHeader.Data,
+		Len:  sliceHeader.Len,
 	}
-
-	// 将字符串结构指针转换为string
 	return *(*string)(unsafe.Pointer(&strHeader))
+	//return unsafe.String(unsafe.SliceData(b), len(b))
 }
 
-// BytesToBytes 零拷贝将字符串转换为[]byte
-func BytesToBytes(s []byte, offset int) []byte {
-	// 获取字节切片的底层结构指针
-	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&s))
-
-	// 构造字节切片的底层结构，复用字符串的Data和Len
+// ZeroCopyBytesToBytes: zero copy []byte->[]byte
+func ZeroCopyBytesToBytes(b []byte) []byte {
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
 	newSliceHeader := reflect.SliceHeader{
-		Data: sliceHeader.Data + uintptr(offset),
-		Len:  sliceHeader.Len - offset,
-		Cap:  sliceHeader.Cap - offset,
+		Data: sliceHeader.Data,
+		Len:  sliceHeader.Len,
+		Cap:  sliceHeader.Len,
 	}
-
-	// 将切片结构指针转换为[]byte
 	return *(*[]byte)(unsafe.Pointer(&newSliceHeader))
+}
+
+func ZeroCopyBytesToBytes121(b []byte) []byte {
+	return unsafe.Slice(unsafe.SliceData(b), len(b))
 }
 
 // 验证这样使用没发生拷贝，估计runtime做过优化
@@ -63,7 +58,7 @@ func BytesParam(b []byte) {
 
 func main() {
 	// 1、[]byte -> string 的零拷贝测试
-	var b1 = []byte("12345678901234567890")
+	var b1 = []byte("123456789012345678901234567890")
 	slice1Header := (*reflect.SliceHeader)(unsafe.Pointer(&b1))
 	fmt.Printf("origin byte array：%+v\n", slice1Header)
 	fmt.Printf("\t%p\n", unsafe.Pointer(slice1Header.Data))
@@ -73,8 +68,8 @@ func main() {
 	fmt.Println(b1)
 
 	// 1) 普通拷贝
-	l := len(b1)
-	s1 := string(b1[4:l])
+	//l := len(b1)
+	s1 := string(b1[4:16])
 	string1Header := (*reflect.StringHeader)(unsafe.Pointer(&s1))
 	fmt.Printf("non zero copied string ：%+v\n", string1Header)
 	fmt.Printf("\t%p\n", unsafe.Pointer(string1Header.Data))
@@ -82,7 +77,7 @@ func main() {
 	fmt.Printf("\t%s\n", s1)
 
 	// 2) 零拷贝
-	s2 := BytesToString(b1, 4)
+	s2 := ZeroCopyBytesToString(b1[4:16])
 	string2Header := (*reflect.StringHeader)(unsafe.Pointer(&s2))
 	fmt.Printf("zero copied：%+v\n", string2Header)
 	fmt.Printf("\t%p\n", unsafe.Pointer(string2Header.Data))
@@ -92,26 +87,44 @@ func main() {
 
 	// 2. []byte->[]byte
 	// 1）普通拷贝
-	b2 := []byte(string(b1[4:l]))
+	b2 := []byte(string(b1[4:16]))
 	slice2Header := (*reflect.SliceHeader)(unsafe.Pointer(&b2))
-	fmt.Printf("non zero copied byte array ：%+v\n", slice2Header)
+	fmt.Printf("non zero copied byte array b2：%+v\n", slice2Header)
 	fmt.Printf("\t%p\n", unsafe.Pointer(slice2Header.Data))
 	fmt.Printf("\t%d\n", slice2Header.Len)
 	fmt.Printf("\t%d\n", slice2Header.Cap)
 	fmt.Println(b2)
 
 	// 2）zero copy
-	b3 := BytesToBytes(b1, 4)
+	b3 := ZeroCopyBytesToBytes(b1[4:16])
 	slice3Header := (*reflect.SliceHeader)(unsafe.Pointer(&b3))
-	fmt.Printf("zero copied byte array ：%+v\n", slice3Header)
+	fmt.Printf("zero copied byte array b3 ：%+v\n", slice3Header)
 	fmt.Printf("\t%p\n", unsafe.Pointer(slice3Header.Data))
 	fmt.Printf("\t%d\n", slice3Header.Len)
 	fmt.Printf("\t%d\n", slice3Header.Cap)
 	fmt.Println(b3)
+
+	// 3) zero copy: 这个方法不太行，data/len是对的，但是Cap沿用旧的是不行的
+	b4 := b1[4:16]
+	slice4Header := (*reflect.SliceHeader)(unsafe.Pointer(&b4))
+	fmt.Printf("zero copied byte array b3 ：%+v\n", slice4Header)
+	fmt.Printf("\t%p\n", unsafe.Pointer(slice4Header.Data))
+	fmt.Printf("\t%d\n", slice4Header.Len)
+	fmt.Printf("\t%d\n", slice4Header.Cap)
+	fmt.Println(b4)
+
+	// 4) zero copy: 这个方法可行
+	b5 := ZeroCopyBytesToBytes121(b1[4:16])
+	slice5Header := (*reflect.SliceHeader)(unsafe.Pointer(&b5))
+	fmt.Printf("zero copied byte array b5 ：%+v\n", slice5Header)
+	fmt.Printf("\t%p\n", unsafe.Pointer(slice5Header.Data))
+	fmt.Printf("\t%d\n", slice5Header.Len)
+	fmt.Printf("\t%d\n", slice5Header.Cap)
+	fmt.Println(b5)
 	fmt.Printf("\n\n")
 
 	// 3.验证 []byte[offset:] 会不会发生拷贝
-	BytesParam(b1[4:])
+	BytesParam(b1[4:16])
 
 	// 2. String 的 零拷贝测试
 	/*var s2 = "1234567890xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxuyyyyyy"
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module go-training
 
-go 1.18
+go 1.20
 
 require (
 	github.com/agiledragon/gomonkey/v2 v2.3.1
